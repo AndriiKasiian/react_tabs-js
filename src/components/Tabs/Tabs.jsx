48a56c80aa1bc171be853fef92365b4bb8be6f15
Andrii Kasiian <kasianandrij@gmail.com>
2024-11-11 14:14:05 +0100

    develop

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,26 @@
-export const Tabs = () => {};
+export const Tabs = ({ tabs, activeId, onTabSelected, activeTab }) => {
+  return (
+    <div>
+      <ul>
+        {tabs.map(tab => (
+          <li data-cy="Tab" className={tab.id === activeId ? 'is-active' : ''}>
+            <a
+              href={`#${tab.id}`}
+              data-cy="TabLink"
+              onClick={() => {
+                if (tab.id !== activeId) {
+                  onTabSelected(tab.id);
+                }
+              }}
+            >
+              {tab.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <div className="block" data-cy="TabContent">
+        {`${activeTab.content}`}
+      </div>
+    </div>
+  );
+};
